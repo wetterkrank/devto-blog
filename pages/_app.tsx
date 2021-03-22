889cb1799b7cb30342a2a0ba2dd26d2d5a7915e1
Alex Antsiferov <wetterkrank@gmail.com>
2021-03-22 23:39:06 +0100

    style for code blocks enabled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 
 import '../styles/global.css'
+import 'highlight.js/styles/railscasts.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
