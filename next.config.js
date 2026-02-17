a5f513b60b13871ace207013e5e60bc81d290d8a
Alexander Antsiferov <wetterkrank@gmail.com>
2026-02-17 22:32:37 +0100

    change build settings to prevent hitting the rate limit

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@
   experimental: {
     // Set to prevent multiple simultaneous getStaticProps calls
     workerThreads: false,
-    cpus: 1
+    cpus: 1,
+    staticGenerationMaxConcurrency: 1, // 1 page at a time per worker
+    staticGenerationRetryCount: 3,     // retries failed page generation
   },
 }
 
